548fa50a806308cbc455bcae86b4fc99308f49a8
Olobam <t.achten@web.de>
2018-01-25 02:10:21 +0100

    Update SL-Utility.lua

--- SL-Utility.lua
+++ SL-Utility.lua
@@ -44,14 +44,8 @@ function Initi:__init()
 	SLU:Menu("Load", "|SL| Loader")
 	SLU.Load:Boolean("LA", "Load Activator", true)
 	SLU.Load:Info("as^dasc", "")
-	SLU.Load:Boolean("LSK", "Load SkinChanger", true)
-	SLU.Load:Info("0.3", "")
 	SLU.Load:Boolean("LAL", "Load AutoLevel", true)
 	SLU.Load:Info("0.4", "")
-	SLU.Load:Boolean("LH", "Load Humanizer", true)
-	SLU.Load:Info("0.6.", "")
-	SLU.Load:Boolean("LRLI", "Load Real life info", true)
-	SLU.Load:Info("0.6yc", "")
 	SLU.Load:Info("0.7.", "You will have to press 2f6")
 	SLU.Load:Info("0.8.", "to apply the changes")
 	
@@ -79,152 +73,6 @@ function SLOrbc:__init()
 		end
 	end)
 	
-end
-
-
-class 'Humanizer'
-
-function Humanizer:__init()
-
-self.bCount = 0
-self.bCount1 = 0
-self.lastCommand = 0
-self.lastspell = 0
-
-	SLU:SubMenu("Hum", "|SL| Humanizer")
-	SLU.Hum:Boolean("Draw", "Draw blocked movements", true)
-	SLU.Hum:Boolean("Draw1", "Draw blocked spells", true)
-	SLU.Hum:Boolean("enable", "Use Movement Limiter", true)
-	SLU.Hum:Boolean("enable1", "Use SpellCast Limiter", true)
-	SLU.Hum:Slider("Horizontal", "Horizontal (Drawings)", 0, 0, GetResolution().x, 10)
-	SLU.Hum:Slider("Vertical", "Vertical (Drawings)", 0, 0, GetResolution().y, 10)
-	SLU.Hum:Menu("ML", "Movement Limiter")
-	SLU.Hum.ML:Slider("lhit", "Max. Movements in Last Hit", 6, 1, 20, 1)
-	SLU.Hum.ML:Slider("lclear", "Max. Movements in Lane Clear", 6, 1, 20, 1)
-	SLU.Hum.ML:Slider("harass", "Max. Movements in Harass", 7, 1, 20, 1)
-	SLU.Hum.ML:Slider("combo", "Max. Movements in Combo", 8, 1, 20, 1)
-	SLU.Hum.ML:Slider("perm", "Persistant Max. Movements", 7, 1, 20, 1)
-	SLU.Hum:Menu("SPC", "SpellCast Limiter")
-	SLU.Hum.SPC:Slider("blhit", "Max. Spells in LastHit", 1, 1, 8, 1)
-	SLU.Hum.SPC:Slider("blclear", "Max. Spells in LaneClear", 1, 1, 8, 1)
-	SLU.Hum.SPC:Slider("bharass", "Max. Spells in Harass", 2, 1, 8, 1)
-	SLU.Hum.SPC:Slider("bcombo", "Max. Spells in Combo", 3, 1, 8, 1)
-	SLU.Hum.SPC:Slider("bperm", "Persistant Max. Spells", 2, 1, 8, 1)
-	
- Callback.Add("IssueOrder", function(order) self:IssueOrder(order) end)
- Callback.Add("SpellCast", function(spell) self:SpellCast(spell) end)
- Callback.Add("Draw", function() self:Draw() end)
-end
-
-function Humanizer:moveEvery()
-	if Mode == "Combo" then
-		return 1 / SLU.Hum.ML.combo:Value()
-	elseif Mode == "LastHit" then
-		return 1 / SLU.Hum.ML.lhit:Value()
-	elseif Mode == "Harass" then
-		return 1 / SLU.Hum.ML.harass:Value()
-	elseif Mode == "LaneClear" then
-		return 1 / SLU.Hum.ML.lclear:Value()
-	else
-		return 1 / SLU.Hum.ML.perm:Value()
-	end
-end
-
-function Humanizer:Spells()
-	if Mode == "Combo" then
-		return 1 / SLU.Hum.SPC.bcombo:Value()
-	elseif Mode == "LastHit" then
-		return 1 / SLU.Hum.SPC.blhit:Value()
-	elseif Mode == "Harass" then
-		return 1 / SLU.Hum.SPC.bharass:Value()
-	elseif Mode == "LaneClear" then
-		return 1 / SLU.Hum.SPC.blclear:Value()
-	else
-		return 1 / SLU.Hum.SPC.bperm:Value()
-	end
-end
-
-function Humanizer:IssueOrder(order)
-	if order.flag == 2 and SLU.Hum.enable:Value() then
-		if os.clock() - self.lastCommand < self:moveEvery() then
-		  BlockOrder()
-		  self.bCount = self.bCount + 1
-		else
-		  self.lastCommand = os.clock()
-		end
-	end
-end
-
-function Humanizer:SpellCast(spell)
-	if SLU.Hum.enable1:Value() then
-		if os.clock() - self.lastspell < self:Spells() then
-		  BlockCast()
-		  self.bCount1 = self.bCount1 + 1
-		else
-		  self.lastspell = os.clock()
-		end
-	end
-end
-
-function Humanizer:Draw()
-	if SLU.Hum.Draw:Value() then
-  		DrawText("Blocked Movements : "..tostring(self.bCount),25,SLU.Hum.Horizontal:Value(),SLU.Hum.Vertical:Value(),ARGB(255,159,242,12))
-	end
-	if SLU.Hum.Draw1:Value() then
-  		DrawText("Blocked Spells : "..tostring(self.bCount1),25,SLU.Hum.Horizontal:Value(),SLU.Hum.Vertical:Value()+20,ARGB(255,159,242,12))
-	end
-end
-
-
-class 'Reallifeinfo'
-
-function Reallifeinfo:__init()
-	SLU:Menu("Date", "|SL| Real life info")
-	SLU.Date:Menu("DDA", "Draw Date")
-	SLU.Date.DDA:Boolean("DrawDate", "Draw Current Date", true)
-	SLU.Date.DDA:Slider("Horizontal", "Horizontal (Drawings)", GetResolution().x*.9, 0, GetResolution().x, 10)
-	SLU.Date.DDA:Slider("Vertical", "Vertical (Drawings)", 60, 0, GetResolution().y, 10)
-	SLU.Date.DDA:ColorPick("ColorPick", "Color Pick - Date", {255,226,255,18})
-	SLU.Date:Menu("DD", "Draw Day")
-	SLU.Date.DD:Boolean("DrawDay", "Draw Current Day", true)
-	SLU.Date.DD:Slider("Horizontal", "Horizontal (Drawings)", GetResolution().x*.9, 0, GetResolution().x, 10)
-	SLU.Date.DD:Slider("Vertical", "Vertical (Drawings)", 80, 0, GetResolution().y, 10)
-	SLU.Date.DD:ColorPick("ColorPick", "Color Pick - Day", {255,226,255,18})
-	SLU.Date:Menu("DM", "Draw Month")
-	SLU.Date.DM:Boolean("DrawMonth", "Draw Current Month", true)
-	SLU.Date.DM:Slider("Horizontal", "Horizontal (Drawings)", GetResolution().x*.9, 0, GetResolution().x, 10)
-	SLU.Date.DM:Slider("Vertical", "Vertical (Drawings)", 100, 0, GetResolution().y, 10)
-	SLU.Date.DM:ColorPick("ColorPick", "Color Pick - Month", {255,226,255,18})
-	SLU.Date:Menu("DY", "Draw Year")
-	SLU.Date.DY:Boolean("DrawYear", "Draw Current Year", true)
-	SLU.Date.DY:Slider("Horizontal", "Horizontal (Drawings)", GetResolution().x*.9, 0, GetResolution().x, 10)
-	SLU.Date.DY:Slider("Vertical", "Vertical (Drawings)", 120, 0, GetResolution().y, 10)
-	SLU.Date.DY:ColorPick("ColorPick", "Color Pick - Year", {255,226,255,18})
-	SLU.Date:Menu("DT", "Draw Time")
-	SLU.Date.DT:Boolean("DrawTime", "Draw Current Time", true)
-	SLU.Date.DT:Slider("Horizontal", "Horizontal (Drawings)", GetResolution().x*.9, 0, GetResolution().x, 10)
-	SLU.Date.DT:Slider("Vertical", "Vertical (Drawings)", 140, 0, GetResolution().y, 10)
-	SLU.Date.DT:ColorPick("ColorPick", "Color Pick - Time", {255,226,255,18})
-	
-	Callback.Add("Draw", function() self:EnableDraw() end)
-end
-
-function Reallifeinfo:EnableDraw()
-	if SLU.Date.DD.DrawDay:Value() then
-		DrawText("Current Day     : "..os.date("%A"), 15, SLU.Date.DD.Horizontal:Value(), SLU.Date.DD.Vertical:Value(), SLU.Date.DD.ColorPick:Value())
-	end
-	if SLU.Date.DDA.DrawDate:Value() then
-		DrawText("Current Date    : "..os.date("%x", os.time()), 15, SLU.Date.DDA.Horizontal:Value(), SLU.Date.DDA.Vertical:Value(), SLU.Date.DDA.ColorPick:Value())
-	end
-	if SLU.Date.DM.DrawMonth:Value() then				
-		DrawText("Current Month : "..os.date("%B"), 15, SLU.Date.DM.Horizontal:Value(), SLU.Date.DM.Vertical:Value(), SLU.Date.DM.ColorPick:Value())
-	end
-	if SLU.Date.DY.DrawYear:Value() then
-		DrawText("Current Year   : "..os.date("%Y"), 15, SLU.Date.DY.Horizontal:Value(), SLU.Date.DY.Vertical:Value(), SLU.Date.DY.ColorPick:Value())
-	end
-	if SLU.Date.DT.DrawTime:Value() then
-		DrawText("Current Time   : "..os.date("*t").hour.." : "..os.date("*t").min.." : "..os.date("*t").sec, 15, SLU.Date.DT.Horizontal:Value(), SLU.Date.DT.Vertical:Value(), SLU.Date.DT.ColorPick:Value())
-	end
 end
 
 
@@ -262,29 +110,6 @@ function AutoLevel:Do()
 	end
 end
 
-
-class 'SkinChanger'
-
-function SkinChanger:__init()
-
-	SLU:SubMenu("S", "|SL| Skin")
-	SLU.S:Boolean("uS", "Use Skin", true)
-	SLU.S:Slider("sV", "Skin Number", 0, 0, 15, 1)
-	
-	local cSkin = 0
-	
-	Callback.Add("Tick", function() self:Change() end)
-end
-
-function SkinChanger:Change()
-	if SLU.S.uS:Value() and SLU.S.sV:Value() ~= cSkin then
-		HeroSkinChanger(myHero,SLU.S.sV:Value()) 
-		cSkin = SLU.S.sV:Value()
-	elseif not SLU.S.uS:Value() and cSkin ~= 0 then
-		HeroSkinChanger(myHero,0)
-		cSkin = 0 
-	end
-end
 
 --Activator
 class 'Activator'
